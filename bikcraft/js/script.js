46f607185975abdaa8650db231bea2890d508da4
fabiochexx <fabiochexx@MacBook-Pro-de-Fabio.local>
2020-02-05 09:59:37 -0300

    Inclusão do projeto Bikcraft

---
+++ bikcraft/js/script.js
@@ -0,0 +1,20 @@
+if(window.SimpleSlide) {
+
+new SimpleSlide({
+    slide: "quote",
+    auto: true,
+    time: 5000 
+});
+
+new SimpleSlide({
+    slide: "portfolio",
+    auto: true,
+    time: 5000,
+    nav: true
+});
+
+}
+
+if(window.SimpleAnime) {
+    new SimpleAnime();
+}
